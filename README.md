ad9d33b041e6f54227ddfc320e278748b2072829
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-09-26 15:31:41 -0400

    deleted questions.js, fixed questionsquizQuestions, fixed labels, changed css, added correct and wrong input colors, saves initials to upper case, remove redundant code.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# coding_quiz
-Web APIs Challenge: Code Quiz
+# Coding Quiz
+
+## Description
+
+This Coding Quiz is a web-based application that tests your knowledge of coding and programming concepts. It consists of a series of multiple-choice questions, and your goal is to answer as many questions as you can within a limited amount of time.
+
+## Usage
+
+1. Start the Quiz:
+   - Click the "Start Quiz" button to begin the quiz.
+
+2. Timer:
+   - When you start the quiz, a timer will begin counting down from a set amount of time (120 seconds).
+   
+3. Answering Questions:
+   - You will be presented with a series of questions, one at a time.
+   - Select one of the provided answer choices for each question by clicking on it.
+
+4. Incorrect Answers:
+   - If you answer a question incorrectly, time will be subtracted from the timer (10 seconds).
+   
+5. Game Over:
+   - The game ends when one of the following conditions is met:
+     - You have answered all the questions.
+     - The timer reaches 0.
+
+6. Saving Your Score:
+   - After the game is over, you will have the opportunity to save your initials and score.
+     - Click the "Save" button to save your score.
+     - Your initials and score will be stored in local storage.
+     
+7. Restart the Quiz:
+   - If you wish to play again, you can click the "Restart Quiz" button to reset the game and start over.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Local Storage
+
+## Demo
+
+Click here for the live demo
+
+## License
+
+This project is licensed under the MIT License.
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let quizQuestion = document.querySelector('.quiz-question');
-let startQuizBtn = document.querySelector('.start-quiz-btn');
+let startQuizBtn = document.querySelector('.quiz-start-btn');
 let nextBtn = document.querySelector('.next-question');
 let quizStartContainer = document.querySelector('.quiz-start')
 let quizContainer = document.querySelector('.quiz-container');
@@ -29,151 +29,6 @@ let initials;
 let records = [];
 let user = {};
 
-// start timer
-function startTimer() {
-    TIMER = setInterval(() => {
-        timeLeft--;
-        timer.textContent = `You have ${timeLeft} seconds left.`;
-        if (timeLeft <= 0) {
-            stopTimer();
-        }
-        for (let checkbox of checkBoxes) {
-            if (questionIndex >= quizQuestions.length - 1 && checkbox.checked === true) {
-                stopTimer();
-            }
-        }
-    }, 1000)
-};
-
-// stops timer and presents points received
-function stopTimer() {
-    clearInterval(TIMER);
-    setScore();
-    quizScoreContainer.style.display = 'block';
-    quizScore.textContent = `You had ${score} of ${quizQuestions.length} correct.`
-};
-
-// saves initials and score to local storage
-function setScore() {
-    initials = prompt('Finished! Enter your initials to save your score.');
-    user.name = initials;
-    user.finalScore = score;
-    records.push(user);
-    if (records && initials) {
-        localStorage.setItem('records', JSON.stringify(records));
-    }
-};
-
-// collects scores
-function getScores() {
-    let recordCollect = JSON.parse(localStorage.getItem('records'));
-    if (recordCollect) {
-        for (let record of recordCollect) {
-            records.push(record);
-        }
-    }
-};
-
-// randomizes question order
-function shuffleQuestions() {
-    quizQuestions.sort(function () {
-        return 0.5 - Math.random();
-    })
-};
-
-shuffleQuestions();
-
-quizQuestion.textContent = quizQuestions[questionIndex].question;
-
-optionElementA.textContent = quizQuestions[questionIndex].optionA;
-optionElementB.textContent = quizQuestions[questionIndex].optionB;
-optionElementC.textContent = quizQuestions[questionIndex].optionC;
-optionElementD.textContent = quizQuestions[questionIndex].optionD;
-
-// event listeners
-startQuizBtn.addEventListener('click', startQuiz);
-restartQuizBtn.addEventListener('click', restartQuiz);
-
-// if statement to go through each question
-function nextQuestion() {
-    for (let checkbox of checkBoxes) {
-        if (questionIndex < quizQuestions.length - 1 && checkbox.checked) {
-            questionIndex++;
-            resetQuestions();
-        }
-    }
-    quizQuestion.textContent = quizQuestions[questionIndex].question;
-
-    optionElementA.textContent = quizQuestions[questionIndex].optionA;
-    optionElementB.textContent = quizQuestions[questionIndex].optionB;
-    optionElementC.textContent = quizQuestions[questionIndex].optionC;
-    optionElementD.textContent = quizQuestions[questionIndex].optionD;
-};
-
-//  calls functions from start button
-function startQuiz() {
-    startTimer();
-    disableCheckboxes();
-    checkAnswers();
-    getScores();
-    scoresLinkContainer.style.display = 'none';
-    quizStartContainer.style.display = 'none';
-    quizContainer.style.display = 'block';
-    timerContainer.style.display = 'block';
-};
-
-// reloads page
-function restartQuiz() {
-    document.location.reload();
-}
-
-// resets the checkboxes for restarting the quiz
-function resetQuestions() {
-    for (let checkbox of checkboxes) {
-        checkbox.checked = false;
-        checkbox.disabled = false;
-        checkbox.nextSibling.style.opacity = '1';
-    }
-    feedback.textContent = '';
-    feedback.classList.remove('correct', 'wrong');
-};
-
-// disabled checkboxes after being clicked
-function disableCheckboxes() {
-    for (let checkbox of checkBoxes) {
-        checkbox.addEventListener('click', (e) => {
-            if (e.target.checked) {
-                for (let unchecked of checkboxes) {
-                    if (!unchecked.checked) {
-                        unchecked.disabled = true;
-                        unchecked.nextSibling.style.opacity = '0.5';
-                    }
-                    checkbox.disabled = true;
-                }
-            }
-        })
-    }
-};
-
-// checks to see if selection is correct or incorrect
-function checkAnswers() {
-    for (i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('click', (e) => {
-            if (e.target.nextSibling.dataset.value === quizQuestions[questionIndex].correctAnswer) { 
-                score ++;
-                feedback.textContent = 'That is correct!';
-                feedback.classList.add('correct');
-            } else {
-                feedback.textContent = 'That is wrong!';
-                feedback.classList.add('wrong');
-                timeLeft -= 10;
-            }
-            setTimeout(function () {
-                nextQuestion();
-            }, 1000);
-        })
-    }
-};
 
 
 
@@ -260,22 +115,180 @@ const quizQuestions = [
 
     // Question #9
     {
-        question: "How is JavaScript called?",
-        optionA: "<nav>",
-        optionB: "<html>",
-        optionC: "<script>",
-        optionD: "<style>",
-        correctAnswer: "optionC"
+        question: "How can we change the background color of an element?",
+        optionA: "color",
+        optionB: "background-color",
+        optionC: "Both A and B",
+        optionD: "None of the above",
+        correctAnswer: "optionB"
     },
 
     // Question #10
     {
-        question: "How is a tag ended?",
-        optionA: "</>",
-        optionB: "<>",
-        optionC: "<,>",
-        optionD: "<->",
-        correctAnswer: "optionA"
+        question: "JavaScript is a ____ language?",
+        optionA: "Object-oriented",
+        optionB: "Functional programming",
+        optionC: "Object-based",
+        optionD: "All of the above",
+        correctAnswer: "optionC"
     }
 
 ];
+
+
+
+// start timer
+function startTimer() {
+    console.log("Timer started");
+    TIMER = setInterval(() => {
+        timeLeft--;
+        timer.textContent = `You have ${timeLeft} seconds left.`;
+        if (timeLeft <= 0) {
+            stopTimer();
+        }
+        for (let checkbox of checkBoxes) {
+            if (questionIndex >= quizQuestions.length - 1 && checkbox.checked === true) {
+                stopTimer();
+            }
+        }
+    }, 1000)
+};
+
+// stops timer and presents points received
+function stopTimer() {
+    clearInterval(TIMER);
+    setScore();
+    quizScoreContainer.style.display = 'block';
+    quizScore.textContent = `You had ${score} of ${quizQuestions.length} correct.`
+};
+
+// saves initials and score to local storage
+function setScore() {
+    initials = prompt('Finished! Enter your initials to save your score.');
+    initials = initials.toUpperCase();
+    user.name = initials;
+    user.finalScore = score;
+    records.push(user);
+    if (records && initials) {
+        localStorage.setItem('records', JSON.stringify(records));
+    }
+};
+
+// collects scores
+function getScores() {
+    let recordCollect = JSON.parse(localStorage.getItem('records'));
+    if (recordCollect) {
+        for (let record of recordCollect) {
+            records.push(record);
+        }
+    }
+};
+
+// randomizes question order
+function shuffleQuestions() {
+    quizQuestions.sort(function () {
+        return 0.5 - Math.random();
+    })
+};
+
+shuffleQuestions();
+
+quizQuestion.textContent = quizQuestions[questionIndex].question;
+
+optionElementA.textContent = quizQuestions[questionIndex].optionA;
+optionElementB.textContent = quizQuestions[questionIndex].optionB;
+optionElementC.textContent = quizQuestions[questionIndex].optionC;
+optionElementD.textContent = quizQuestions[questionIndex].optionD;
+
+// event listeners
+startQuizBtn.addEventListener('click', startQuiz);
+restartQuizBtn.addEventListener('click', restartQuiz);
+
+// if statement to go through each question
+function nextQuestion() {
+    for (let checkbox of checkBoxes) {
+        checkbox.classList.remove('correct', 'wrong')
+        if (questionIndex < quizQuestions.length - 1 && checkbox.checked) {
+            questionIndex++;
+            resetQuestions();
+        }
+    }
+    quizQuestion.textContent = quizQuestions[questionIndex].question;
+
+    optionElementA.textContent = quizQuestions[questionIndex].optionA;
+    optionElementB.textContent = quizQuestions[questionIndex].optionB;
+    optionElementC.textContent = quizQuestions[questionIndex].optionC;
+    optionElementD.textContent = quizQuestions[questionIndex].optionD;
+};
+
+//  calls functions from start button
+function startQuiz() {
+    startTimer();
+    disableCheckboxes();
+    checkAnswers();
+    getScores();
+    scoresLinkContainer.style.display = 'none';
+    quizStartContainer.style.display = 'none';
+    quizContainer.style.display = 'block';
+    timerContainer.style.display = 'block';
+};
+
+// reloads page
+function restartQuiz() {
+    document.location.reload();
+}
+
+// resets the checkboxes for restarting the quiz
+function resetQuestions() {
+    for (let checkbox of checkBoxes) {
+        checkbox.checked = false;
+        checkbox.disabled = false;
+        checkbox.nextSibling.style.opacity = '1';
+    }
+    feedback.textContent = '';
+    feedback.classList.remove('correct', 'wrong');
+};
+
+// disabled checkboxes after being clicked
+function disableCheckboxes() {
+    for (let checkbox of checkBoxes) {
+        checkbox.addEventListener('click', (e) => {
+            if (e.target.checked) {
+                for (let unchecked of checkBoxes) {
+                    if (!unchecked.checked) {
+                        unchecked.disabled = true;
+                        unchecked.nextSibling.style.opacity = '0.5';
+                    }
+                    checkbox.disabled = true;
+                }
+            }
+        })
+    }
+};
+
+// checks to see if selection is correct or incorrect
+function checkAnswers() {
+    for (i = 0; i < checkBoxes.length; i++) {
+        checkBoxes[i].addEventListener('click', (e) => {
+            const selectedCheckbox = e.target;
+            const isCorrectAnswer = selectedCheckbox.nextSibling.dataset.value === quizQuestions[questionIndex].correctAnswer;
+
+            if (isCorrectAnswer) { 
+                score++;
+                selectedCheckbox.classList.add('correct');
+                feedback.textContent = 'That is correct!';
+                feedback.classList.add('correct');
+            } else {
+                selectedCheckbox.classList.add('wrong');
+                feedback.textContent = 'That is wrong!';
+                feedback.classList.add('wrong');
+                timeLeft -= 10;
+            }
+
+            setTimeout(function () {
+                nextQuestion();
+            }, 1000);
+        });
+    }
+}
+
